83e1c1a82fb2e635719026ce77d8a8158e332b31
Clement Jeulin <clement93jeulin@gmail.com>
2022-09-16 11:30:38 +0200

    Ajout de la fonction de Hashage du password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,15 @@
-//importation du modèle de la base de données
+//-------------------------------------LES IMPORTS-------------------------------------//
+//IMPORTATION DE : bcrypt afin de hasher le password
+const bcrypt = require ('bcrypt')
+console.log('CONTENU DE BCRYPT')
+console.log(bcrypt)
+//IMPORTATION DE : express
 const express = require('express');
 const app = express();
+//IMPORTATION DE : module CORS, permettant d'éviter les erreurs relatives à l'origine de la requête 
 var cors = require('cors');
 app.use(cors());
+//IMPORTATION DE : Modèle user 
 const User = require('../models/user');
 
 console.log("CONTENU DE : user")
@@ -21,17 +28,24 @@ exports.signup = (req, res, next) => {
     console.log(req.body.password)
 
     //Le Hachage du Mot de Passe 
-    
-    const user = new User ({
-        email : req.body.email,
-        password : req.body.password,
+    bcrypt.hash(req.body.password, 10) // L'algorithme de hash sera executé 10 fois 
+    .then((hash) =>{
+        // ce qui sera enregistré dans MongoDB
+        const user = new User ({
+            email : req.body.email,
+            password : hash,
+        });
+        console.log("Contenu du nouvel User :")
+        console.log(user)
+
+        //Envoyer le user dans la base de données MongoDB
+        user
+        .save()
+        .then(()=>
+            res.status(201).json({message: "Utilisateur crée et sauvegardé"})
+            )
+            .catch((error) => res.status(400).json({error}).send())
+
     })
- 
-    console.log("Contenu du nouvel User :")
-    console.log(user)
-    
-    user
-    .save()
-    .then(() => res.status(201).json({message: "User Created and Saved"}))
-    .catch((error) => res.status(500).json({error}))
+    .catch((error) => res.status(500).json({error}).send(console.log(error)))
 };
